7b96784498232847bbeca43c44c8232579fc911b
Peter Qwarnstrom <peter.qwarnstrom@gmail.com>
2014-12-13 10:12:15 +0100

    First ajax call against api

---
+++ HappyMeter.Web/HappyMeter.Web/Scripts/App/ApiRepository.js
@@ -0,0 +1,23 @@
+﻿function getMoods() {
+	var response;
+	jQuery.get("http://happymeter.apphb.com/Skandia/Moods/", function (response) {
+		console.log(response);
+	});
+
+	return response;
+}
+
+function getMoodsByAjax() {
+	var data = "hej";
+	jQuery.ajax({
+		url: 'http://happymeter.apphb.com/Skandia/Moods/',
+		type: 'GET',
+		dataType: 'jsonp',
+		async: false,
+		success: function (response) {
+			data = response;
+		}
+	});
+
+	return data;
+}
